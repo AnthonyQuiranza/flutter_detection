3c2edac901acdd33ef0258c95cea8a46348d93b6
Anthony <anthony.quiranza@upec.edu.ec>
2022-03-20 16:29:07 -0500

    Menú principal

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -9,14 +9,16 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Menú Principal'),
+        elevation: 20,
+        toolbarTextStyle: TextStyle(fontWeight: FontWeight.w800),
         centerTitle: true,
         backgroundColor: Colors.green,
       ),
-      body: _menuList(),
+      body: _menuList(context),
     );
   }
 
-  Widget _menuList() {
+  Widget _menuList(BuildContext context) {
     return Center(
         child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -58,9 +60,59 @@ class HomePage extends StatelessWidget {
           subtitle:
               Text('¿Deseas saber más sobre nosotros y nuestro proyecto?'),
           trailing: Icon(Icons.navigate_next, size: 50),
-          onTap: () {},
+          onTap: () => _quienesSomosAlert(context),
+        ),
+        Divider(
+          thickness: 1,
+          height: 20,
+          color: Colors.green,
         ),
       ],
     ));
+  }
+
+  _quienesSomosAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: Text('¿Quiénes somos?'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://krugercorp.com/wp-content/uploads/2022/02/KRUGER-icon-naranja-03.png'),
+                  radius: 40,
+                  backgroundColor: Colors.white,
+                ),
+                Text('Este es un proyecto en su versión Beta 0.2'),
+                ListTile(
+                  title: Text('Contáctanos'),
+                  leading: Icon(Icons.contact_mail),
+                ),
+                ListTile(
+                  title: Text('Visítanos en nuestro sitio web'),
+                  leading: Icon(Icons.web),
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text(
+                    'Cancelar',
+                    style: TextStyle(color: Colors.green),
+                  )),
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('OK', style: TextStyle(color: Colors.green)))
+            ],
+          );
+        });
   }
 }
